6e7110221a1a529d8f2fc0ffd4577170bfb9794e
Radosław Dabrowski <radoslaw.dabrowski@coig.pl>
2019-03-21 14:51:38 +0100

    Api example for zuul

---
+++ spring-boot-zuul/poc-server/src/main/java/com/coig/intraid/poc/entities/Person.java
@@ -0,0 +1,20 @@
+package com.coig.intraid.poc.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
