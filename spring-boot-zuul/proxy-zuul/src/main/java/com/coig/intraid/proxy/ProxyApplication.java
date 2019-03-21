5d2d54fd9ef46bece6091b735498c48fc971aa4d
Radosław Dabrowski <kontakt@radoslawdabrowski.pl>
2019-03-21 16:42:19 +0100

    CORS

--- spring-boot-zuul/proxy-zuul/src/main/java/com/coig/intraid/proxy/ProxyApplication.java
+++ spring-boot-zuul/proxy-zuul/src/main/java/com/coig/intraid/proxy/ProxyApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -17,6 +20,16 @@ public class ProxyApplication {
     @Bean
     public ProcessingFilter processingFilter() {
         return new ProcessingFilter();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("POST", "OPTIONS", "DELETE", "GET");
+            }
+        };
     }
 
 }
