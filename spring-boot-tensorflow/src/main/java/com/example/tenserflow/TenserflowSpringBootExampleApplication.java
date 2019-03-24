3385dc163b95ed5afd1cb44f7f810dd1b6ec132e
rdabrowski <kontakt@radoslawdabrowski.pl>
2019-03-24 12:01:32 +0100

    Added spring-boot-tensorflow

---
+++ spring-boot-tensorflow/src/main/java/com/example/tenserflow/TenserflowSpringBootExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.tenserflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenserflowSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenserflowSpringBootExampleApplication.class, args);
+	}
+}
---
+++ spring-boot-tensorflow/src/test/java/com/example/tenserflow/TenserflowSpringBootExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.tenserflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TenserflowSpringBootExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
